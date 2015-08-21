9f795bb979a311acac9edd164f4c72f7389a653a
Lukasz Ratajczak <l.ratajczak@ogicom.pl>
2015-08-21 21:00:35 +0200

    new sql folder

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 
 app=Flask(__name__)
+app.config["DEBUG"]=True
+
 
 @app.route('/')
 def hello_world():
 	msg="hello world"
 	return render_template("index.html",msg=msg)
+
+@app.route('/name/<name>')
+def is_name(name):
+	if name == 'lukasz':
+		return "hello {}, how are you".format(name)
+	else: return "name not found",404	
 
 @app.route('/integer/<int:value>')
 def int_type(value):
@@ -14,12 +22,12 @@ def int_type(value):
 
 @app.route('/float/<float:value>')
 def float_type(value):
-	print value + 1.1
+	print "value:",value + 1.1
 	return "correct"
 
 @app.route('/path/<path:value>')
 def path_type(value):
-	print value
+	print "value:",value
 	return "correct"
 
 if __name__=='__main__':
